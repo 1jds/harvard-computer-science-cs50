5f2e311176f18e967411bae6195dec3a1fa5b25f
James <jamesdstacey2@gmail.com>
2023-12-03 00:20:26 +1100

    added sentimenal-mario-more

---
+++ Week 6 Python/Problem Set 6/sentimental-mario-more/mario.py
@@ -0,0 +1,32 @@
+def main():
+    while True:
+        try:
+            height = int(input("Height: "))
+            if height > 0 and height < 9:
+                break
+            else:
+                print("Enter a number between 1 and 8")
+        except:
+            print("Enter a number between 1 and 8")
+
+    i = 1
+    while i <= height:
+        print_spaces(height - i)
+        print_hashes(i)
+        print("  ", end="")
+        print_hashes(i)
+        print()
+        i = i + 1
+
+
+def print_spaces(n):
+    for i in range(n):
+        print(" ", end="")
+
+
+def print_hashes(n):
+    for i in range(n):
+        print("#", end="")
+
+
+main()
