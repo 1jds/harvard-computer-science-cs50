2552ac14a6f118d4023d51a870ecc69431f60ddc
James <jamesdstacey2@gmail.com>
2023-12-05 21:46:58 +1100

    final version of dna.py

--- Week 6 Python/Problem Set 6/dna/dna.py
+++ Week 6 Python/Problem Set 6/dna/dna.py
@@ -1,58 +1,63 @@
-# Removed hard-coding of input files, now need to work on hard-coding of subsequences...
-
 import csv
 import sys
 from typing import List, Dict
 
+
 def main():
     # Check for command-line usage
-    if not len(sys.argv) == 3 or not sys.argv[1][-4:] == ".csv" or not sys.argv[2][-4:] == ".txt":
+    if (
+        not len(sys.argv) == 3
+        or not sys.argv[1][-4:] == ".csv"
+        or not sys.argv[2][-4:] == ".txt"
+    ):
         print("Usage: python dna.py data.csv sequence.txt")
 
-    # Read database file into a variable
-    list_from_csv_file: List[Dict[str, int]] = []  # Or, as we would say in JavaScript, 'array'! With some typing here...
+    # Read database .csv file into a list of dicts / array of objects
+    list_from_csv_file: List[
+        Dict[str, int]
+    ] = []  # honestly, just using types here to experiment a bit
     csv_file = open(sys.argv[1], "r", encoding="utf8")
     my_csv_reader_dictionary = csv.DictReader(csv_file)
-
     for row in my_csv_reader_dictionary:
         int_row: Dict[str, float] = {}
         for column in row:
-            if not column == 'name':
+            if not column == "name":
                 int_row[column] = int(row[column])
             else:
                 int_row[column] = row[column]
         list_from_csv_file.append(int_row)
-    # print(list_from_csv_file)
+    list_of_column_names = my_csv_reader_dictionary.fieldnames
     csv_file.close()
 
     # Read DNA sequence file into a variable
     individuals_sequence_file = open(sys.argv[2], "r", encoding="utf8")
     sequence_string = individuals_sequence_file.readline()
-    # print(sequence_string)
     individuals_sequence_file.close()
 
-    # TODO: Find longest match of each STR in DNA sequence
-    # STRs are as follows in the small file: AGATC, AATG, TATC
-    longest_match_for_AGATC = longest_match(sequence_string, 'AGATC')
-    longest_match_for_AATG = longest_match(sequence_string, 'AATG')
-    longest_match_for_TATC = longest_match(sequence_string, 'TATC')
-    # print(longest_match_for_AGATC)
-    # print(longest_match_for_AATG)
-    # print(longest_match_for_TATC)
+    # Find longest match of each STR in DNA sequence
+    list_of_longest_matches = []
+    for i in range(1, len(list_of_column_names)):
+        list_of_longest_matches.append(
+            longest_match(sequence_string, list_of_column_names[i])
+        )
 
-    # TODO: Check database for matching profiles
+    # Check database for matching profiles
     dna_owner = "No match"
-    # iterate through this list_from_csv_file
-    for dict in list_from_csv_file: # 'dict', or as we would say in JavaScript, 'object'!
-        if dict['AGATC'] == longest_match_for_AGATC and dict['AATG'] == longest_match_for_AATG and dict['TATC'] == longest_match_for_TATC:
-            dna_owner = dict['name']
-
+    for dict in list_from_csv_file:
+        match = True
+        for key in range(0, len(list_of_longest_matches)):
+            if not dict[list_of_column_names[key + 1]] == list_of_longest_matches[key]:
+                match = False
+        if match:
+            dna_owner = dict["name"]
     print(dna_owner)
 
     return
 
 
-def longest_match(sequence, subsequence):
+def longest_match(
+    sequence, subsequence
+):  # This function was made by the CS50 staff. The main function above was implemented by me
     """Returns length of longest run of subsequence in sequence."""
 
     # Initialize variables
@@ -62,7 +67,6 @@ def longest_match(sequence, subsequence):
 
     # Check each character in sequence for most consecutive runs of subsequence
     for i in range(sequence_length):
-
         # Initialize count of consecutive runs
         count = 0
 
@@ -70,7 +74,6 @@ def longest_match(sequence, subsequence):
         # If a match, move substring to next potential match in sequence
         # Continue moving substring and checking for matches until out of consecutive matches
         while True:
-
             # Adjust substring start and end
             start = i + count * subsequence_length
             end = start + subsequence_length
