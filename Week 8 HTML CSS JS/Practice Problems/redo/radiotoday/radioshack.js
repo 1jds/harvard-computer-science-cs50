b4fc2aa7fed26402bd396b31d172545abbe281a4
James <jamesdstacey2@gmail.com>
2023-12-17 03:09:53 +1100

    refactored radioshack.js

--- Week 8 HTML CSS JS/Practice Problems/redo/radiotoday/radioshack.js
+++ Week 8 HTML CSS JS/Practice Problems/redo/radiotoday/radioshack.js
@@ -15,11 +15,15 @@ const scrollObserver = new IntersectionObserver(entries => {
         else if (entry.isIntersecting && entry.target.classList.contains('will-fade-in')) {
             entry.target.classList.add('fade-in')
         }
+        else if (entry.isIntersecting && entry.target.classList.contains('will-slide-up')) {
+            entry.target.classList.add('slide-up')
+        }
     })
 }, options)
 
-const itemsToAnimate = document.querySelectorAll('.slide-in-from-right, .slide-in-from-left, .will-fade-in').forEach(item => {
+const itemsToAnimate = document.querySelectorAll('.slide-in-from-right, .slide-in-from-left, .will-fade-in, .will-slide-up').forEach(item => {
     scrollObserver.observe(item)
 })
+
 
 
