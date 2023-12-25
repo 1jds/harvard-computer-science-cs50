06271b0b0f6bb42ab43f6b405b54e8f351e4781b
James <jamesdstacey2@gmail.com>
2023-12-26 02:50:03 +1100

    completed Lab 9 Flask server

--- Week 9 Flask/Lab 9/birthdays/app.py
+++ Week 9 Flask/Lab 9/birthdays/app.py
@@ -25,8 +25,6 @@ def after_request(response):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-
-        # TODO: Add the user's entry into the database
         form_data_name = request.form.get('name')
         form_data_month = request.form.get('month')
         form_data_day = request.form.get('day')
@@ -37,37 +35,53 @@ def index():
                 form_data_month,
                 form_data_day,
             )
-
-        return redirect("/")
+            return redirect("/")
 
     else:
-        # TODO: Display the entries in the database on index.html
         list_of_birthdays_from_db = db.execute("SELECT * FROM birthdays;")
-
-        # print(list_of_birthdays_from_db)
-        # [
-        #     {'id': 1, 'name': 'Harry', 'month': 7, 'day': 31},
-        #     {'id': 2, 'name': 'Ron', 'month': 3, 'day': 1},
-        #     {'id': 3, 'name': 'Hermione', 'month': 9, 'day': 19}
-        # ]
         return render_template("index.html", birthdays_list=list_of_birthdays_from_db)
 
-@app.route("/delete", methods=["GET", "POST"])
+@app.route("/delete", methods=["POST"])
 def delete_entry():
-    if request.method == "POST":
-        birthday_id = request.form.get('id')
-        if birthday_id:
-            db.execute(
-                "DELETE FROM birthdays WHERE id=?",
-                birthday_id,
-            )
-            # print("I found the data", birthday_id)
-            # print("I'm going to sleep now...")
-            # sleep(5)
-            # print("I've woken up now...")
-            return redirect("/")
-        else:
-            print("No id field found in form data")
-            return redirect("/")
+    birthday_id = request.form.get('id')
+    if birthday_id:
+        db.execute(
+            "DELETE FROM birthdays WHERE id=?",
+            birthday_id,
+        )
+        return ("Item deleted")
     else:
+        return ("No id field found in form data")
+
+@app.route("/edit", methods=["POST"])
+def edit_entry():
+    form_data_name = request.form.get('name')
+    form_data_month = request.form.get('month')
+    form_data_day = request.form.get('day')
+    form_data_id = request.form.get('id')
+    print("Here are the details", form_data_name, form_data_month, form_data_day, form_data_id)
+    if form_data_id: # A better way to do this would be to construct a dynamic query, but that could be liable to SQL injection...
+        if form_data_name:
+            print("got here #1")
+            db.execute(
+                "UPDATE birthdays SET name=? WHERE id=?",
+                form_data_name,
+                form_data_id,
+            )
+        if form_data_month:
+            print("got here #2")
+            db.execute(
+                "UPDATE birthdays SET month=? WHERE id=?",
+                form_data_month,
+                form_data_id,
+            )
+        if form_data_day:
+            print("got here #3")
+            db.execute(
+                "UPDATE birthdays SET day=? WHERE id=?",
+                form_data_day,
+                form_data_id,
+            )
         return redirect("/")
+    else:
+        return ("No id found to update birthday")
