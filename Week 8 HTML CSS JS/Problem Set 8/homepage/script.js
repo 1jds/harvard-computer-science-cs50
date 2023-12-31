ef4daca5d91815749b3d0da6310297658b43c270
James <jamesdstacey2@gmail.com>
2024-01-01 05:19:20 +1100

    completed bootstrap homepage test

---
+++ Week 8 HTML CSS JS/Problem Set 8/homepage/script.js
@@ -0,0 +1,18 @@
+
+
+
+const bgb = document.querySelector('#big-green-btn')
+bgb.addEventListener('click', (e) => {
+    alert("Well done, you clicked the big button! :)");
+})
+
+
+const lgb = document.querySelector('#little-green-btn')
+lgb.addEventListener('click', (e) => {
+    alert("I would give you my resume, but this is just a test page for using Bootstrap! Please refer to my github :)");
+})
+
+const lgb2 = document.querySelector('#little-green-btn-2')
+lgb2.addEventListener('click', (e) => {
+    alert("I would give you my resume, but this is just a test page for using Bootstrap! Please refer to my github :)");
+})
