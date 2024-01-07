74a7bf76670010f6cee66b5b8fa5b31b7082789c
James <jamesdstacey2@gmail.com>
2024-01-07 23:17:34 +1100

    added universal styling from Kevin

--- Week 10 Project/cs50-final-project/src/App.jsx
+++ Week 10 Project/cs50-final-project/src/App.jsx
@@ -1,12 +1,50 @@
 import { useState } from "react";
-import "./App.css";
+import "./index.css";
 import Navbar from "./Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1>Hello World!</h1>
+      <main className="content-grid">
+        <h1>Hello World!</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          sapiente fugiat asperiores nisi dicta voluptates ea quasi, atque
+          minima, aspernatur iste nesciunt aliquam! Iusto iure eveniet explicabo
+          dicta nobis ad? Enim ducimus velit commodi voluptatem, eum animi culpa
+          minus repellat totam, labore ut repudiandae incidunt modi blanditiis
+          facere sunt fugiat aperiam quam deleniti eligendi tenetur illum
+          aspernatur dicta nihil! A. Quibusdam, est! Debitis maxime tenetur
+          ipsam, asperiores adipisci nostrum, nihil mollitia perspiciatis
+          necessitatibus non at quibusdam deleniti sunt quos. Dignissimos, vel
+          dolore voluptatibus cum est alias molestias iusto eos voluptatum.
+        </p>
+        <p className="breakout">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          sapiente fugiat asperiores nisi dicta voluptates ea quasi, atque
+          minima, aspernatur iste nesciunt aliquam! Iusto iure eveniet explicabo
+          dicta nobis ad? Enim ducimus velit commodi voluptatem, eum animi culpa
+          minus repellat totam, labore ut repudiandae incidunt modi blanditiis
+          facere sunt fugiat aperiam quam deleniti eligendi tenetur illum
+          aspernatur dicta nihil! A. Quibusdam, est! Debitis maxime tenetur
+          ipsam, asperiores adipisci nostrum, nihil mollitia perspiciatis
+          necessitatibus non at quibusdam deleniti sunt quos. Dignissimos, vel
+          dolore voluptatibus cum est alias molestias iusto eos voluptatum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          sapiente fugiat asperiores nisi dicta voluptates ea quasi, atque
+          minima, aspernatur iste nesciunt aliquam! Iusto iure eveniet explicabo
+          dicta nobis ad? Enim ducimus velit commodi voluptatem, eum animi culpa
+          minus repellat totam, labore ut repudiandae incidunt modi blanditiis
+          facere sunt fugiat aperiam quam deleniti eligendi tenetur illum
+          aspernatur dicta nihil! A. Quibusdam, est! Debitis maxime tenetur
+          ipsam, asperiores adipisci nostrum, nihil mollitia perspiciatis
+          necessitatibus non at quibusdam deleniti sunt quos. Dignissimos, vel
+          dolore voluptatibus cum est alias molestias iusto eos voluptatum.
+        </p>
+      </main>
     </>
   );
 }
